c0262a10d00abfc72db1c7b29580e6d38d91fa42
Shaun Smith <shaun.smith@oracle.com>
2023-08-24 18:11:56 -0400

    added copyright notices

--- src/main/java/com/example/micronaut/entity/Teacher.java
+++ src/main/java/com/example/micronaut/entity/Teacher.java
@@ -1,4 +1,11 @@
-package com.example.micronaut.entity;
+/*
+ * Copyright © 2023, Oracle and/or its affiliates.
+ * 
+ * Released under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl/
+ * or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
+ */
+
+ package com.example.micronaut.entity;
 
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.data.annotation.GeneratedValue;
